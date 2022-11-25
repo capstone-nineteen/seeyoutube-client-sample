0180bfea01e24ace059003ed89858ad66ba7f5c9
최수정 <babeeboo2000@gmail.com>
2022-11-25 23:18:41 +0900

    [Init] 프로젝트 생성

---
+++ SeeYoutubeSample/SeeYoutubeSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeYoutubeSample
+//
+//  Created by 최수정 on 2022/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
